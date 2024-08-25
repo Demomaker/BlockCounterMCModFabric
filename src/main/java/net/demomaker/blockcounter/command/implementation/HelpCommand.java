919b49cb1aa91f4316bdcd49e2d73df9293759e8
Mike Bédard <mike01bedard@gmail.com>
2024-08-25 14:42:05 -0400

    apply 1.18 fixes

--- src/main/java/net/demomaker/blockcounter/command/implementation/HelpCommand.java
+++ src/main/java/net/demomaker/blockcounter/command/implementation/HelpCommand.java
@@ -4,7 +4,6 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import net.demomaker.blockcounter.adapter.argumentbuilder.ServerCommandArgumentBuilder;
 import net.demomaker.blockcounter.adapter.servercommand.ServerCommand;
 import net.demomaker.blockcounter.adapter.servercommand.ServerCommandContext;
-import net.demomaker.blockcounter.adapter.servercommand.ServerCommandRegistryAccess;
 import net.demomaker.blockcounter.command.config.SourceType;
 import net.demomaker.blockcounter.identity.CommandExecutionConfig;
 import net.demomaker.blockcounter.identity.CommandExecutionConfigResolver;
@@ -15,14 +14,14 @@ import net.demomaker.blockcounter.util.TranslationText;
 public class HelpCommand extends ServerCommand {
   public static final String DEFAULT_COMMAND_NAME = "help";
   public static final String COMMAND_NAME = "command_name";
-  public static ServerCommandArgumentBuilder getDefaultServerCommandFormat(ServerCommandRegistryAccess registryAccess, ServerCommand Command) {
+  public static ServerCommandArgumentBuilder getDefaultServerCommandFormat(ServerCommand Command) {
     return new ServerCommandArgumentBuilder()
         .beginCommand(DEFAULT_COMMAND_NAME, Command)
         .addStringArgument(COMMAND_NAME, ModCommands.HELP_COMMAND)
         .endCommand();
   }
 
-  public static ServerCommandArgumentBuilder getTranslatedServerCommandFormat(ServerCommandRegistryAccess registryAccess, ServerCommand Command) {
+  public static ServerCommandArgumentBuilder getTranslatedServerCommandFormat(ServerCommand Command) {
     TranslationText.TRANSLATED_TO_DEFAULT_MAP.put(TranslationText.commandHelp.getString(), DEFAULT_COMMAND_NAME);
 
     return new ServerCommandArgumentBuilder()
--- src/main/java/net/demomaker/blockcounter/util/TranslationText.java
+++ src/main/java/net/demomaker/blockcounter/util/TranslationText.java
@@ -3,33 +3,38 @@ package net.demomaker.blockcounter.util;
 import java.util.HashMap;
 import java.util.Map;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class TranslationText {
 
   public static final Map<String, String> TRANSLATED_TO_DEFAULT_MAP = new HashMap<>();
-  public static Text title = Text.translatable("blockcounter.title");
-  public static Text description = Text.translatable("blockcounter.description");
-  public static Text numberOfBlocks = Text.translatable("blockcounter.numberOfBlocks");
-  public static Text total = Text.translatable("blockcounter.total");
-  public static Text commandPreCommand = Text.translatable("blockcounter.command.precommand");
-  public static Text commandCountBlocks = Text.translatable("blockcounter.command.countblocks");
-  public static Text commandSetPosition = Text.translatable("blockcounter.command.setposition");
-  public static Text commandHelp = Text.translatable("blockcounter.command.help");
-  public static Text commandArgumentFirstPosition = Text.translatable("blockcounter.command.arguments.firstposition");
-  public static Text commandArgumentSecondPosition = Text.translatable("blockcounter.command.arguments.secondposition");
-  public static Text commandArgumentBlockName = Text.translatable("blockcounter.command.arguments.blockname");
-  public static Text commandArgumentCommandName = Text.translatable("blockcounter.command.arguments.commandname");
-  public static Text commandHelpMessageCountBlocks = Text.translatable("blockcounter.commandhelpmessage.countblocks");
-  public static Text commandHelpMessageSetPosition = Text.translatable("blockcounter.commandhelpmessage.setposition");
-  public static Text commandHelpMessageCommon = Text.translatable("blockcounter.commandhelpmessage.common");
-  public static Text commandHelpMessageOneLinerCountBlocks = Text.translatable("blockcounter.commandhelpmessageoneliner.countblocks");
-  public static Text commandHelpMessageOneLinerSetPosition = Text.translatable("blockcounter.commandhelpmessageoneliner.setposition");
-  public static Text commandHelpMessageOneLinerCommon = Text.translatable("blockcounter.commandhelpmessageoneliner.common");
+  public static Text title = createTranslatableText("blockcounter.title");
+  public static Text description = createTranslatableText("blockcounter.description");
+  public static Text numberOfBlocks = createTranslatableText("blockcounter.numberOfBlocks");
+  public static Text total = createTranslatableText("blockcounter.total");
+  public static Text commandPreCommand = createTranslatableText("blockcounter.command.precommand");
+  public static Text commandCountBlocks = createTranslatableText("blockcounter.command.countblocks");
+  public static Text commandSetPosition = createTranslatableText("blockcounter.command.setposition");
+  public static Text commandHelp = createTranslatableText("blockcounter.command.help");
+  public static Text commandArgumentFirstPosition = createTranslatableText("blockcounter.command.arguments.firstposition");
+  public static Text commandArgumentSecondPosition = createTranslatableText("blockcounter.command.arguments.secondposition");
+  public static Text commandArgumentBlockName = createTranslatableText("blockcounter.command.arguments.blockname");
+  public static Text commandArgumentCommandName = createTranslatableText("blockcounter.command.arguments.commandname");
+  public static Text commandHelpMessageCountBlocks = createTranslatableText("blockcounter.commandhelpmessage.countblocks");
+  public static Text commandHelpMessageSetPosition = createTranslatableText("blockcounter.commandhelpmessage.setposition");
+  public static Text commandHelpMessageCommon = createTranslatableText("blockcounter.commandhelpmessage.common");
+  public static Text commandHelpMessageOneLinerCountBlocks = createTranslatableText("blockcounter.commandhelpmessageoneliner.countblocks");
+  public static Text commandHelpMessageOneLinerSetPosition = createTranslatableText("blockcounter.commandhelpmessageoneliner.setposition");
+  public static Text commandHelpMessageOneLinerCommon = createTranslatableText("blockcounter.commandhelpmessageoneliner.common");
 
   public static String getDefaultEnglishEquivalent(String commandName) {
     if(commandName == null) {
       return null;
     }
     return TRANSLATED_TO_DEFAULT_MAP.get(commandName);
+  }
+
+  private static Text createTranslatableText(String key) {
+    return new TranslatableText(key);
   }
 }
