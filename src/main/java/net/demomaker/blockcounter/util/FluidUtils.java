1c68373181e786fc48289d83bca370b99efb1f11
Mike Bédard <mike01bedard@gmail.com>
2024-08-25 15:08:12 -0400

    fix fluid

--- src/main/java/net/demomaker/blockcounter/util/FluidUtils.java
+++ src/main/java/net/demomaker/blockcounter/util/FluidUtils.java
@@ -1,17 +1,14 @@
 package net.demomaker.blockcounter.util;
 
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariantAttributes;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class FluidUtils {
   public static Text getFluidName(Fluid fluid) {
-    // Get the fluid's registry name
-    Identifier fluidId = Registry.FLUID.getId(fluid);
     // Create a translation key using the fluid's registry name
-    return new TranslatableText("fluid." + fluidId.getNamespace() + "." + fluidId.getPath());
+    return Text.of(FluidVariantAttributes.getName(FluidVariant.of(fluid)));
   }
 
   public static String getFluidNameAsString(Fluid fluid) {
