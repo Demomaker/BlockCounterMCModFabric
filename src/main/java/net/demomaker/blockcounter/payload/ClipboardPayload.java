f428001ce2fd1de4f9ddfc93d91c69dec1dfa9b7
Mike Bédard <mike01bedard@gmail.com>
2024-09-17 19:53:55 -0400

    fix problem

--- src/main/java/net/demomaker/blockcounter/payload/ClipboardPayload.java
+++ src/main/java/net/demomaker/blockcounter/payload/ClipboardPayload.java
@@ -3,9 +3,7 @@ package net.demomaker.blockcounter.payload;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 public class ClipboardPayload {
     @NotNull
-    public static final Identifier CLIPBOARD_PAYLOAD_ID = Objects.requireNonNull(Identifier.of("blockcounter", "clipboard_payload_id"));
+    public static final Identifier CLIPBOARD_PAYLOAD_ID = new Identifier("blockcounter", "clipboard_payload_id");
 }
