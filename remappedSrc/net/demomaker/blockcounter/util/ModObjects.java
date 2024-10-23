9ee0cd0cdc8742edb4b142c31e8645b0b3f3d14c
Mike Bédard <mike01bedard@gmail.com>
2024-10-22 20:33:59 -0400

    Delete remappedSrc from git

--- remappedSrc/net/demomaker/blockcounter/util/ModObjects.java
+++
@@ -1,7 +0,0 @@
-package net.demomaker.blockcounter.util;
-
-import net.demomaker.blockcounter.player.PlayerConfigs;
-
-public class ModObjects {
-  public static PlayerConfigs playerConfigs = new PlayerConfigs();
-}
