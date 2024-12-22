1c511b6096d04d1bc953176d105e2d3fe48d1c9d
Mike Bédard <mike01bedard@gmail.com>
2024-12-22 11:04:34 -0500

    make client specific code client-only
    # Conflicts:
    #	src/main/java/net/demomaker/blockcounter/main/BlockCounter.java

--- src/main/java/net/demomaker/blockcounter/main/BlockCounter.java
+++ src/main/java/net/demomaker/blockcounter/main/BlockCounter.java
@@ -2,13 +2,17 @@ package net.demomaker.blockcounter.main;
 
 import net.demomaker.blockcounter.payload.ClipboardPayload;
 import net.demomaker.blockcounter.util.ModObjects;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.MinecraftServer;
 
@@ -19,18 +23,35 @@ public class BlockCounter implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        initializeServer();
+
+        if(FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            initializeClient();
+        }
+    }
+
+    public void initializeServer() {
         CommandRegistrationCallback.EVENT.register(ModCommands::register);
         ServerPlayConnectionEvents.DISCONNECT.register(ModCommands::disconnect);
         ServerPlayConnectionEvents.JOIN.register(ModCommands::join);
         AttackBlockCallback.EVENT.register(ModCommands::blockLeftClick);
         ServerLifecycleEvents.SERVER_STARTED.register(BlockCounter::onServerStart);
+        PayloadTypeRegistry.playS2C().register(ClipboardPayload.ID, ClipboardPayload.CODEC);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public void initializeClient() {
         ClientLifecycleEvents.CLIENT_STARTED.register(BlockCounter::onClientStart);
-        ClientPlayNetworking.registerGlobalReceiver(ClipboardPayload.CLIPBOARD_PAYLOAD_ID, (client, handler, buf, responseSender) -> {
-            MinecraftClient localClient = ModObjects.minecraftClient;
-            localClient.keyboard.setClipboard(buf.readString());
+        ClientPlayNetworking.registerGlobalReceiver(ClipboardPayload.ID, (payload, context) -> {
+            MinecraftClient client = ModObjects.minecraftClient;
+            if(client == null) {
+                return;
+            }
+            client.keyboard.setClipboard(payload.clipboardText());
         });
     }
 
+    @Environment(EnvType.CLIENT)
     private static void onClientStart(MinecraftClient minecraftClient) {
         ModObjects.minecraftClient = minecraftClient;
     }
